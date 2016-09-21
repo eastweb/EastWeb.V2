f7fb87ed6c2e1de2adfd59ca3fa76ce506166bd6
liuyi-j <liuyi_j@yahoo.com>
2016-09-20 21:20:55 -0500

    Incomplete TRMM3B42_New download

---
+++ src/version2/prototype/download/TRMM3B42_New/TRMM3B42_NewListDatesFiles.java
@@ -0,0 +1,124 @@
+package version2.prototype.download.TRMM3B42_New;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import version2.prototype.Config;
+import version2.prototype.DataDate;
+import version2.prototype.ErrorLog;
+import version2.prototype.PluginMetaData.DownloadMetaData;
+import version2.prototype.ProjectInfoMetaData.ProjectInfoFile;
+import version2.prototype.ProjectInfoMetaData.ProjectInfoPlugin;
+import version2.prototype.download.DownloadUtils;
+import version2.prototype.download.ListDatesFiles;
+
+// @Author: Yi Liu
+
+public class TRMM3B42_NewListDatesFiles extends ListDatesFiles
+{
+    public TRMM3B42_NewListDatesFiles(DataDate startDate, DownloadMetaData data, ProjectInfoFile project) throws IOException
+    {
+        super(startDate, data, project);
+    }
+
+    @Override
+    protected Map<DataDate, ArrayList<String>> ListDatesFilesFTP() {
+        return null;
+    }
+
+    @Override
+    protected Map<DataDate, ArrayList<String>> ListDatesFilesHTTP()
+    {
+        HashMap<DataDate, ArrayList<String>> tempMapDatesFiles =  new HashMap<DataDate, ArrayList<String>>();
+
+        final String mHostURL = mData.myHttp.url;
+
+        try
+        {
+            ByteArrayOutputStream folderOutStream = new ByteArrayOutputStream();
+            DownloadUtils.downloadToStream(new URL(mHostURL), folderOutStream);
+
+            List<String> availableYears = Arrays.asList(folderOutStream.toString().split("[\\r\\n]+"));
+
+            outerLoop: for(String paramY : availableYears)
+            {
+                if(Thread.currentThread().isInterrupted())
+                {
+                    break;
+                }
+
+                String yearPattern = "(19|20)\\d\\d";
+                String monthPattern = "(0?[1-9]|1[012])";
+
+                //String datePattern = "((19|20)\\d\\d)(0?[1-9]|1[012])(0?[1-9]|[12][0-9]|3[01])/";
+                Pattern pattern = Pattern.compile(yearPattern);
+                Matcher matcher = pattern.matcher(paramY);
+
+                // URL structure:  host/yyyy/mm
+                if(matcher.find())
+                {
+                    try
+                    {
+                        int year = Integer.parseInt(matcher.group());
+
+                        if (year >= sDate.getYear())
+                        {
+                            String yearFolderURL = mHostURL + String.format("%04d", year);
+
+                            ArrayList<String> monthList = new ArrayList<String>();
+
+                            ByteArrayOutputStream monthFolderOutstream = new ByteArrayOutputStream();
+                            DownloadUtils.downloadToStream(new URL(yearFolderURL), monthFolderOutstream);
+
+                            List<String> availableMonths = Arrays.asList(monthFolderOutstream.toString().split("[\\r\\n]+"));
+
+                            for(String paramM : availableMonths)
+                            {
+                                Pattern patternM = Pattern.compile(monthPattern);
+                                Matcher matcherM = patternM.matcher(paramM);
+
+                                if (matcherM.find())
+                                {
+                                    System.out.println(paramM);
+                                    try
+                                    {
+                                        System.out.println(matcherM.group());
+                                        int month = Integer.parseInt(matcherM.group());
+                                        System.out.println("month: " + month);
+                                    }
+                                    catch(Exception e)
+                                    {
+                                        ErrorLog.add(Config.getInstance(), mData.Title, mData.name, "ModisListDatesFiles.ListDatesFilesHTTP problem while getting file list.", e);
+                                    }
+                                }
+                            }
+
+                        }
+                    }
+                    catch(Exception e)
+                    {
+                        ErrorLog.add(Config.getInstance(), mData.Title, mData.name, "ModisListDatesFiles.ListDatesFilesHTTP problem while getting file list.", e);
+                    }
+                }
+            }
+
+        }
+        catch(Exception e)
+        {
+            ErrorLog.add(Config.getInstance(), mData.Title, mData.name, "ModisListDatesFiles.ListDatesFilesHTTP problem while getting file list.", e);
+        }
+
+
+        return tempMapDatesFiles;
+
+
+    }
+}
---
+++ src/version2/prototype/download/TRMM3B42_New/TestTRMM_NewListDates.java
@@ -0,0 +1,61 @@
+package version2.prototype.download.TRMM3B42_New;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Map;
+
+
+import version2.prototype.DataDate;
+import version2.prototype.PluginMetaData.DownloadMetaData;
+import version2.prototype.PluginMetaData.FTP;
+import version2.prototype.PluginMetaData.HTTP;
+
+public class TestTRMM_NewListDates
+{
+    static public void main(String [ ] args)
+    {
+        String mode = "http";// the protocol type: ftp or http
+        FTP myFtp = null;
+        HTTP myHttp = new HTTP("http://disc2.gesdisc.eosdis.nasa.gov/data/TRMM_L3/TRMM_3B42_Daily.7/");
+        String className = null;
+        String timeZone = null;
+        int filesPerDay = 1;
+        String datePatternStr = "\\d{4}";
+        String fileNamePatternStr =
+                "3B42_daily\\.(\\d{4}d{2}d{2})\\.7\\.nc4";
+
+        //LocalDate ld = LocalDate.parse("Wed Sep 14 00:00:01 CDT 2016", DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss zzz uuuu"));
+        LocalDate ld = LocalDate.parse("Wed Sep 14 00:00:01 CDT 2016", DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss zzz uuuu"));
+
+        DownloadMetaData data = new DownloadMetaData(null, null, null, 25000, true, null,
+                mode, myFtp, myHttp, className, timeZone, filesPerDay,
+                datePatternStr, fileNamePatternStr, ld);
+
+        /* String Title, ArrayList<String> QualityControlMetaData,
+        Integer DaysPerInputData, Integer Resolution,
+        Boolean CompositesContinueIntoNextYear,
+        ArrayList<String> ExtraDownloadFiles,
+        String mode, FTP myFtp, HTTP myHttp,
+        String downloadFactoryClassName, String timeZone,
+        int filesPerDay, String datePatternStr, String fileNamePatternStr,
+        LocalDate originDate
+         */
+
+        TRMM3B42_NewListDatesFiles iList;
+        try {
+            iList = new TRMM3B42_NewListDatesFiles(new DataDate(data.originDate), data, null);
+            Map<DataDate, ArrayList<String>> tempDatesFiles = iList.CloneListDatesFiles();
+
+            for (Map.Entry<DataDate, ArrayList<String>> entry : tempDatesFiles.entrySet())
+            {
+                System.out.println(entry.getKey() + ":/ " +  entry.getValue());
+            }
+
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
