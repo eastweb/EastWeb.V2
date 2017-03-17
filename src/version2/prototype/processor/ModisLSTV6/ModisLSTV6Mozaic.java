caed4a3160f1a31b70072c4379f83d0928b38785
liuyi_j <liuyi_j@yahoo.com>
2017-03-17 07:55:11 -0500

    MODISLSTV6 - Mosaic

--- src/version2/prototype/processor/ModisLSTV6/ModisLSTV6Mozaic.java
+++ src/version2/prototype/processor/ModisLSTV6/ModisLSTV6Mozaic.java
@@ -13,4 +13,20 @@ public class ModisLSTV6Mozaic extends Mozaic{
     protected int[] getBands() {
         return new int[] {1,2, 5, 6};
     }
+
+    @Override
+    protected String getBandNamePattern(){
+        //pattern: SUBDATASET_(1|2|5|6)_NAME
+
+        int bandsLength = bands.length;
+
+        String bandpattern = "SUBDATASET_" + "(" + String.valueOf(bands[0]);
+        for (int i = 1; i<= bandsLength-1; i++)
+        {
+            bandpattern += "|" + String.valueOf(bands[i]);
+        }
+        bandpattern += ")_NAME";
+
+        return bandpattern;
+    }
 }
